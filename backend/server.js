2ade418cf2921360633a65dcbcbe79a45cee884e
Billy Seol <billy@julylifecoach.com>
2026-03-12 16:07:36 -0700

    fix: explicit cors for resources domain

--- backend/server.js
+++ backend/server.js
@@ -39,6 +39,7 @@ const allowedOrigins = [
     'https://www.julylifecoach.com',
     'https://julylifecoach.com',
     'https://french.julylifecoach.com',
+    'https://resources.julylifecoach.com',
     'http://localhost:5173',
     'http://localhost:5174',
 ];
